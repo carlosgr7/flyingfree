cc47b8fbb967c2d77f200e9111cdcb5990f7b997
Carlos <carlosgomezr77@gmail.com>
2025-01-29 08:37:36 +0100

    Creando la clase de la skin de Bird y la clase que manejara las colisiones con fisicas de Bird

---
+++ lib/Bodies/BirdBody.dart
@@ -0,0 +1,9 @@
+import 'package:flame/components.dart';
+import 'package:flame_forge2d/body_component.dart';
+import 'package:flame_forge2d/contact_callbacks.dart';
+
+class BirdBody extends BodyComponent with KeyboardHandler, ContactCallbacks{
+
+
+
+}
--- lib/Characters/Bird.dart
+++ lib/Characters/Bird.dart
@@ -1,7 +1,39 @@
-import 'package:flame/components.dart';
-import 'package:flame_forge2d/body_component.dart';
-import 'package:flame_forge2d/contact_callbacks.dart';
+import 'dart:ui';
 
-class Bird extends BodyComponent with KeyboardHandler, ContactCallbacks{
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flyingfree/Games/FlyingFreeGame.dart';
+
+class Bird extends SpriteAnimationComponent
+    with HasGameReference<FlyingFree>, KeyboardHandler, CollisionCallbacks{
+
+  Bird({required super.position,}) :
+        super(size: Vector2(64,64), anchor: Anchor.center);
+
+  @override
+  void onLoad() {
+    animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('bird1.png'),
+      SpriteAnimationData.sequenced(
+        amount: 4,
+        textureSize: Vector2.all(32),
+        stepTime: 0.12,
+      ),
+    );
+
+    //add(CircleHitbox(collisionType: CollisionType.active));
+  }
+
+  @override render(Canvas canvas){
+    super.render(canvas);
+
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+
+  }
 
 }
